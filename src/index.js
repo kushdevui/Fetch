828923dfab86c2938a4e19ba3c195e687604ce32
Rajat Kumar 2 <rajkumar24@publicisgroupe.net>
2018-10-11 15:57:34 +0530

    Webpack features added are: Css extraction, react transpiling , code splitting, (TODO - Hot module Reloading)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const App = ()=> {
-    return(
-        <div>
-            <h1>"Hello world!!"</h1>
-        </div>
-    )
-}
+import App from './components/app';
 
 ReactDOM.render(<App/> , document.getElementById('root'));
