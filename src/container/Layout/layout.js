8535157c215a17911bdfa1a02ac2ff12e55d7797
Kush Kumar <kuskumar0@publicisgroupe.net>
2018-11-22 17:57:21 +0530

    Fixed review comments

--- src/container/Layout/layout.js
+++ src/container/Layout/layout.js
@@ -7,7 +7,7 @@ import { onResizeAction } from "../../redux/actions/globalActions";
 
 //importing Components 
 import Home from "../../components/Home/home";
-import signup from "../../components/Signup/signup";
+import SignUp from "../../components/Signup/signup";
 
 class Layout extends Component {
     constructor(){
@@ -33,7 +33,7 @@ class Layout extends Component {
             <div className="container-fluid">
                 <Switch>
                     <Route path="/" exact  component={Home}/>
-                    <Route path="/signup" exact component={signup}/>
+                    <Route path="/signup" exact component={SignUp}/>
                 </Switch>
             </div>
         );
